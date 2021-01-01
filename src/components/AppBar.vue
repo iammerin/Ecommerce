2ea5e90d6aba7c21d95f2be2279efb43ef0a864e
leo <leodahal4@gmail.com>
2021-01-01 23:31:31 +0545

    transparent appbar. More design needed!

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,42 +1,75 @@
 <template>
-  <v-app-bar max-height="70" color="primary" dark>
+  <v-app-bar
+    max-height="70"
+    color="transparent"
+    app
+    elevate-on-scroll
+  >
     <v-spacer></v-spacer>
     <v-toolbar-title>
-      <v-btn text rounded to="/">
+      <v-btn
+        text
+        rounded
+        to="/"
+      >
         Ecommerce
       </v-btn>
     </v-toolbar-title>
-    <v-form method="POST" action="#">
+    <v-form
+      method="POST"
+      action="#"
+    >
       <v-text-field
-          filled
-          rounded
-          dense
-          hide-details
-          append-icon="mdi-magnify"
-          single-line
-          @click:append="searchButton"
-          class="ma-5"
-        ></v-text-field>
+        filled
+        rounded
+        dense
+        hide-details
+        append-icon="mdi-magnify"
+        single-line
+        @click:append="searchButton"
+        class="ma-5"
+      ></v-text-field>
     </v-form>
     <div id="nav">
-      <v-btn text rounded to="/categories">Categories</v-btn> |
-      <v-btn text rounded to="/about">About</v-btn>
-      <v-btn text rounded to="/deals">Deals</v-btn>
-      <v-btn text rounded to="/coupons">Coupons</v-btn>
-      <v-btn text rounded to="/stores">Stores</v-btn>
-      <v-chip
-        class="ma-2"
-        outlined
-        dark
-        large
+      <v-btn
+        text
+        rounded
+        v-for="link in routes"
+        :key="link.name"
+        :to="link.link"
       >
-        <v-icon
-          class="mr-2"
-        >
-          mdi-cart
-        </v-icon>
-        Cart
-      </v-chip>
+        {{ link.name }}
+      </v-btn>
+      <v-menu
+        offset-y
+      >
+        <template v-slot:activator="{ attrs, on }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            width="100"
+            text
+          >
+            <v-chip
+              class="ma-2"
+              outlined
+              dark
+            >
+              <v-icon
+                class="mr-2"
+              >
+                mdi-cart
+              </v-icon>
+              Cart
+            </v-chip>
+          </v-btn>
+        </template>
+        <v-list>
+        <v-list-item>
+          <v-list-item-title>No Items on the cart.</v-list-item-title>
+        </v-list-item>
+      </v-list>
+      </v-menu>
     </div>
     <v-spacer></v-spacer>
   </v-app-bar>
@@ -48,6 +81,17 @@ export default {
   methods: {
     searchButton () {
       console.log('clicked')
+    }
+  },
+  data () {
+    return {
+      routes: [
+        { name: 'Categories', link: '/link' },
+        { name: 'About', link: '/about' },
+        { name: 'Deals', link: '/deals' },
+        { name: 'TOA', link: '/terms-and-conditions' },
+        { name: 'Stores', link: '/stores' }
+      ]
     }
   }
 }
---
+++ src/views/TermsAndConditions.vue
@@ -0,0 +1,197 @@
+<template>
+  <v-row
+    align="center"
+    justify="center"
+    class="mt-5"
+  >
+    <v-col cols="8">
+      <v-card>
+        <v-card-title>Terms and Conditions</v-card-title>
+        <v-card-text>
+          <div class="text-h5">
+            Intellectual Propertly
+          </div>
+          <ul class="mt-5">
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+          </ul>
+          <div class="text-h5 mt-8">
+            Termination
+          </div>
+          <ul class="mt-5">
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis diam erat. Duis velit lectus, posuere a blandit sit amet, tempor at lorem. Donec ultricies, lorem sed ultrices interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+          </ul>
+          <div class="text-h5 mt-8">
+            Changes to this agreement
+          </div>
+          <div class="text-subtitle-2 mt-5">
+            We reserve the right, at our sole discretion, to modify or replace these Terms and Conditions by posting the updated terms on the Site. Your continued use of the Site after any such changes constitutes your acceptance of the new Terms and Conditions.
+          </div>
+          <div class="text-h5 mt-8">
+            Contact Us
+          </div>
+          <div class="text-subtitle-2 mt-5">
+            If you have any questions about this Agreement, please contact us filling this
+          <v-dialog
+            v-model="dialog"
+            persistent
+            max-width="600px"
+            scrollable
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                v-bind="attrs"
+                v-on="on"
+                text
+                small
+              >
+                Contact form
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">User Profile</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        label="Legal first name*"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        label="Legal middle name"
+                        hint="example of helper text only on focus"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        label="Legal last name*"
+                        hint="example of persistent helper text"
+                        persistent-hint
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="Email*"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="Password*"
+                        type="password"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                    >
+                      <v-select
+                        :items="['0-17', '18-29', '30-54', '54+']"
+                        label="Age*"
+                        required
+                      ></v-select>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                    >
+                      <v-autocomplete
+                        :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                        label="Interests"
+                        multiple
+                      ></v-autocomplete>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                  <v-textarea
+                    counter
+                    label="Text"
+                    :rules="rules"
+                    :value="value"
+                  ></v-textarea>
+                  </v-row>
+                </v-container>
+                <small>*indicates required field</small>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="dialog = false"
+                >
+                  Dismiss
+                  <v-icon
+                    class="ml-2"
+                  >
+                    mdi-close
+                  </v-icon>
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="dialog = false"
+                >
+                  Send
+                  <v-icon
+                    class="ml-2"
+                    small
+                  >
+                    mdi-send
+                  </v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          .
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: false,
+      rules: [v => v.length >= 50 || 'Minimum 50 characters'],
+      value: 'Describe here.'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
