fd9a98c77535a1b41bd2c8f01e72372f92bac6dd
leo <leodahal4@gmail.com>
2021-01-01 18:10:47 +0545

    changes on deploy script

--- scripts/live-site-deploy.js
+++ scripts/live-site-deploy.js
@@ -5,7 +5,7 @@ const fs = require('fs');
   try {
     await execa('git', ['checkout', '--orphan', 'live-site'])
     console.log('Building...')
-    await execa('npm', ['run', 'build'])
+    await execa('yarn', ['build'])
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build'
     await execa('git', ['--work-tree', folderName, 'add', '--all'])
