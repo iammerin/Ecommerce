d866bdc6cc4ff04719a9b3e3ccb9f04615df725d
leo <leodahal4@gmail.com>
2021-01-08 13:22:57 +0545

    Home page, Featured Sales added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,22 @@
   <div class="home">
     <CourselWithMenu />
     <FeaturedCategories />
+    <FeaturedSales />:
   </div>
 </template>
 
 <script>
 import CourselWithMenu from '@/components/CarouselWithMenu'
 import FeaturedCategories from '@/components/FeaturedCategories'
+import FeaturedSales from '@/components/FeaturedSales'
 // @ is an alias to /src
 
 export default {
   name: 'Home',
   components: {
     CourselWithMenu,
-    FeaturedCategories
+    FeaturedCategories,
+    FeaturedSales
   }
 }
 </script>
