c47ebdbd63af4944a0f36d4cd3184ea5e4bbbda2
leo <leodahal4@gmail.com>
2021-01-08 09:04:34 +0545

    carousel design change

--- src/components/CarouselWithMenu.vue
+++ src/components/CarouselWithMenu.vue
@@ -2,45 +2,46 @@
   <v-row align="center" justify="center" class="mt-2">
     <v-col cols="10">
       <v-row>
-        <v-col cols="3">
-          <v-card
-            class="mx-auto navigationMenu"
-            max-width="500"
-            max-height="700"
-          >
-            <v-list>
-              <v-subheader>Categories</v-subheader>
-              <v-list-group
-                v-for="item in items"
-                :key="item.title"
-                v-model="item.active"
-                no-action
-              >
-                <template v-slot:activator>
-                  <v-list-item-content>
-                    <v-list-item-title v-text="item.title"></v-list-item-title>
-                  </v-list-item-content>
-                </template>
-                <v-list-item
-                  v-for="child in item.items"
-                  :key="child.title"
-                  link
-                  :to="child.title"
-                >
-                  <v-list-item-title v-text="child.title"></v-list-item-title>
-                </v-list-item>
-              </v-list-group>
-            </v-list>
-          </v-card>
-        </v-col>
-        <v-col cols="9">
+        <!-- <v&#45;col cols="3"> -->
+        <!--   <v&#45;card -->
+        <!--     class="mx&#45;auto navigationMenu" -->
+        <!--     max&#45;width="500" -->
+        <!--     max&#45;height="700" -->
+        <!--   > -->
+        <!--     <v&#45;list> -->
+        <!--       <v&#45;subheader>Categories</v&#45;subheader> -->
+        <!--       <v&#45;list&#45;group -->
+        <!--         v&#45;for="item in items" -->
+        <!--         :key="item.title" -->
+        <!--         v&#45;model="item.active" -->
+        <!--         no&#45;action -->
+        <!--       > -->
+        <!--         <template v&#45;slot:activator> -->
+        <!--           <v&#45;list&#45;item&#45;content> -->
+        <!--             <v&#45;list&#45;item&#45;title v&#45;text="item.title"></v&#45;list&#45;item&#45;title> -->
+        <!--           </v&#45;list&#45;item&#45;content> -->
+        <!--         </template> -->
+        <!--         <v&#45;list&#45;item -->
+        <!--           v&#45;for="child in item.items" -->
+        <!--           :key="child.title" -->
+        <!--           link -->
+        <!--           :to="child.title" -->
+        <!--         > -->
+        <!--           <v&#45;list&#45;item&#45;title v&#45;text="child.title"></v&#45;list&#45;item&#45;title> -->
+        <!--         </v&#45;list&#45;item> -->
+        <!--       </v&#45;list&#45;group> -->
+        <!--     </v&#45;list> -->
+        <!--   </v&#45;card> -->
+        <!-- </v&#45;col> -->
+        <v-col cols="12">
+          <v-card elevation="24">
           <v-carousel
             hide-delimiters
             cycle
             interval="6000"
             progress
             progress-color="primary"
-            height="700"
+            height="550"
           >
             <v-carousel-item
               v-for="(item, i) in carouselItems"
@@ -64,7 +65,7 @@
                   </v-row>
                 </template>
                 <v-col cols="12">
-                  <v-sheet color="transparent" height="670" class="d-flex align-content-end flex-wrap" tile>
+                  <v-sheet color="transparent" height="545" class="d-flex align-content-end flex-wrap" tile>
                     <div class="pa-5">
                       <v-btn text
                         :to="item.src"
@@ -80,6 +81,7 @@
               </v-img>
             </v-carousel-item>
           </v-carousel>
+          </v-card>
         </v-col>
       </v-row>
     </v-col>
